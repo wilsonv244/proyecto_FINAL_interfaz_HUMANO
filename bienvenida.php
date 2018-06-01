a2f149ce548ffdc0584d5cfa67719489330760b2
juliocesar123456 <39635794+juliocesar123456@users.noreply.github.com>
2018-06-01 13:08:23 -0500

    juliocesar123456

--- bienvenida.php
+++ bienvenida.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8"> 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap IPW</title>
+    <title>BIENVENIDO</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>

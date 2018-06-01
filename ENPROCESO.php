9bd0eb365eb58e732d281f74251313b482d7d830
wilsonv244 <39635463+wilsonv244@users.noreply.github.com>
2018-05-31 22:24:48 -0500

    finalizacion de la pagina MASAJES
    por el momento doy por finalizado la paguina de RELAJACION

--- ENPROCESO.php
+++ ENPROCESO.php
@@ -17,8 +17,9 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
   </head>
-  <body bgcolor="#717D7E">
+  <body bgcolor="#513D7E">
 <nav class="navbar navbar-inverse navbar-static-top">
 	<div class="container">
 		<div class="navbar-header">
@@ -86,6 +87,7 @@
   ...
 </div>
 </div>
+	 <?php include("comentario.php"); ?>
 	<!--<img src="imagenes\registro\enproceso.jpg" align="">-->
 </body>
 </html>
